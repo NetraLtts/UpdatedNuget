4751589864631ecb27aaa03f9cdfb7eefdefac95
Netravati <Netravati.Hiremath@Ltts.com>
2022-04-12 12:25:43 +0530

    Updated Nuget Packages

---
+++ ClassLibrary_Nuget/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassLibrary_Nuget
+{
+    public class Calculator
+    {
+        public int Add(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
